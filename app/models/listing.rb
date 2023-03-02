243acdd04e460c19dba65b1c818f630eedd43b7e
Namgel Dorjee <dorjee@tutamail.com>
2023-03-02 11:28:36 +0100

    new search-field in listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,12 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title_and_details,
+  against: [ :title, :details ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
